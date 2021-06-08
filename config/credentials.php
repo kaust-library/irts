1cfb40ff754a68b2060e5c0f5133e8ecddbd5d2c
dagrenzer <daryl.grenz@kaust.edu.sa>
2021-06-08 10:33:28 +0300

    Create credentials.php

---
+++ config/credentials.php
@@ -0,0 +1,17 @@
+<?php
+	define('MYSQL_USER', 'irts');
+	define('MYSQL_PW', '');
+	
+	define('REPOSITORY_USER', '');
+	define('REPOSITORY_PW', '');
+
+  define('WOS_API_USER', '');
+	define('WOS_API_PW', '');
+
+	//Metadata Source API Keys
+	define('ELSEVIER_API_KEY', ''); //Elsevier key is used with both Scopus and ScienceDirect
+	
+	define('IEEE_API_KEY', '');
+	
+	define('SHERPA_ROMEO_API_KEY', '');
+
